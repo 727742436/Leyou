aefece282e53e71e9f202ee833a4cdd419628310
727742436 <57586747+727742436@users.noreply.github.com>
2019-12-21 15:17:26 +0800

    Add files via upload

---
+++ leyou/leyou-auth/leyou-auth-common/src/main/java/com/leyou/auth/LeyouAuthCommonApplication.java
@@ -0,0 +1,13 @@
+package com.leyou.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeyouAuthCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouAuthCommonApplication.class, args);
+    }
+
+}
---
+++ leyou/leyou-auth/leyou-auth-common/src/test/java/com/leyou/auth/LeyouAuthCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyouAuthCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
