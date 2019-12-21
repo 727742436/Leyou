8c39a20faf94b70789018e226661eb9ad9efa10d
727742436 <57586747+727742436@users.noreply.github.com>
2019-12-21 15:37:55 +0800

    Add files via upload

---
+++ leyou/leyou-search/src/main/java/leyou/search/controller/SearchController.java
@@ -0,0 +1,28 @@
+package leyou.search.controller;
+
+import com.leyou.common.pojo.PageResult;
+import leyou.search.pojo.Goods;
+import leyou.search.pojo.SearchRequest;
+import leyou.search.pojo.SearchResult;
+import leyou.search.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class SearchController {
+    @Autowired
+    private SearchService searchService;
+
+    @PostMapping("page")
+    public ResponseEntity<SearchResult> search(@RequestBody SearchRequest request){
+        SearchResult result= this.searchService.search(request);
+        if (result==null || CollectionUtils.isEmpty(result.getItems())){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+}
