0cc75e66a20fab2427cae690f08a4f7e968144f2
727742436 <57586747+727742436@users.noreply.github.com>
2019-12-21 15:18:50 +0800

    Add files via upload

---
+++ leyou/leyou-cart/src/test/java/com/leyou/cart/LeyouCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyouCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
